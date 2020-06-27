78dfc5d85d3e17597991374d9dbdf0523c7dc0cc
chrisstanford <chris.j.stanford@gmail.com>
2020-06-26 18:52:12 -0700

    sped things up with conditional tokens

--- frontend/src/features/room/Tokens.js
+++ frontend/src/features/room/Tokens.js
@@ -9,14 +9,22 @@ export const Tokens = ({
 }) => {
     if (card.aspectRatio < 1) return(
         <div>
-            <Token type="threat" amount={0} left={"10%"} top={"0%"} adjustVisible={adjustVisible}></Token>
+            {0 ? <Token type="threat" amount={0} left={"10%"} top={"0%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="willpower" amount={0} left={"10%"} top={"25%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="attack" amount={0} left={"10%"} top={"50%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="defense" amount={0} left={"10%"} top={"75%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="resource" amount={0} left={"55%"} top={"0%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="damage" amount={0} left={"55%"} top={"25%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="progress" amount={0} left={"55%"} top={"50%"} adjustVisible={adjustVisible}></Token>:null}
+            {0 ? <Token type="time" amount={0} left={"55%"} top={"75%"} adjustVisible={adjustVisible}></Token>:null}
+            {/* <Token type="threat" amount={0} left={"10%"} top={"0%"} adjustVisible={adjustVisible}></Token>
             <Token type="willpower" amount={13} left={"10%"} top={"25%"} adjustVisible={adjustVisible}></Token>
             <Token type="attack" amount={8} left={"10%"} top={"50%"} adjustVisible={adjustVisible}></Token>
             <Token type="defense" amount={0} left={"10%"} top={"75%"} adjustVisible={adjustVisible}></Token>
             <Token type="resource" amount={3} left={"55%"} top={"0%"} adjustVisible={adjustVisible}></Token>
             <Token type="damage" amount={-1} left={"55%"} top={"25%"} adjustVisible={adjustVisible}></Token>
             <Token type="progress" amount={1} left={"55%"} top={"50%"} adjustVisible={adjustVisible}></Token>
-            <Token type="time" amount={-3} left={"55%"} top={"75%"} adjustVisible={adjustVisible}></Token>
+            <Token type="time" amount={-3} left={"55%"} top={"75%"} adjustVisible={adjustVisible}></Token> */}
         </div>
     )
 }
