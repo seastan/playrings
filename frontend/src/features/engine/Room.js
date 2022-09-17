f48cc3d971bb924526827dc905afaa0d2c3255fe
Chris Stanford <chris.j.stanford@gmail.com>
2022-09-16 22:02:53 -0400

    litlle change

--- frontend/src/features/engine/Room.js
+++ frontend/src/features/engine/Room.js
@@ -95,7 +95,7 @@ export const Room = ({ slug }) => {
 
   console.log('Rendering Room',myUserId);
   // console.log("plugin room",plugin)
-  // if (plugin === null) return (<div className="text-white m-4">Plugin not found.</div>);
+  //if (plugin === null) return (<div className="text-white m-4">Loading...</div>);
 
   if (roomName !== slug) return (<div></div>);
   else {
