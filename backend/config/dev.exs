3788de177f617cd591b6438d632bdf180bc9333a
Eric Lloyd <erlloyd@gmail.com>
2023-06-04 23:03:19 -0500

    Try a docker setup for local development

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -5,7 +5,7 @@ config :dragncards, DragnCards.Repo,
   username: "postgres",
   password: "postgres",
   database: "dragncards_dev",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOSTNAME", "localhost"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
