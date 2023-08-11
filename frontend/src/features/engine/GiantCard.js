4d9aeee1cac1c7e60a2b0878c0d3036e59771bda
seastan <chris.j.stanford@gmail.com>
2023-08-11 01:37:33 -0400

    active card set to null when card groupId changes to avoid active card remaining defined hafter being shuffled into deck

--- frontend/src/features/engine/GiantCard.js
+++ frontend/src/features/engine/GiantCard.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import useProfile from "../../hooks/useProfile";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useVisibleFace } from "./hooks/useVisibleFace";
 import { useVisibleFaceSrc } from "./hooks/useVisibleFaceSrc";
 import { useActiveCardId } from "./hooks/useActiveCardId";
+import { setActiveCardId } from "../store/playerUiSlice";
 
 export const GiantCard = React.memo(({}) => {
   const user = useProfile();
+  const dispatch = useDispatch();
   const touchAction = useSelector(state => state?.playerUi?.touchAction);
   const activeCardId = useActiveCardId();
+  const activeCardGroupId = useSelector(state => state?.gameUi?.game?.cardById?.[activeCardId]?.groupId);
+  const prevActiveCardGroupIdRef = useRef(activeCardGroupId);
   const visibleFace = useVisibleFace(activeCardId);
   const screenLeftRight = useSelector(state => state?.playerUi?.screenLeftRight);
   const visibleFaceSrc = useVisibleFaceSrc(activeCardId);
   console.log("Rendering GiantCard", visibleFace, visibleFaceSrc);
+
+  useEffect(() => {
+  
+    // Check if activeCardId is not null
+    if (activeCardId !== null) {
+      // Check if activeCardGroupId has changed from its previous value
+      if (activeCardGroupId !== prevActiveCardGroupIdRef.current && prevActiveCardGroupIdRef.current) {
+        // Dispatch the action to set activeCardId to null
+        dispatch(setActiveCardId(null));
+      }
+    }
+
+    // Update the ref with the current value of activeCardGroupId
+    prevActiveCardGroupIdRef.current = activeCardGroupId;
+  }, [activeCardId, activeCardGroupId, dispatch]);
 
   if (activeCardId && !touchAction) {
     var height = visibleFace.height >= visibleFace.width ? "70vh" : "50vh";
