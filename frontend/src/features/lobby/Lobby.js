081c0864b24e04779605bf30f5b9369dcad658ba
seastan <chris.j.stanford@gmail.com>
2023-08-20 01:21:30 -0400

    bugfix

--- frontend/src/features/lobby/Lobby.js
+++ frontend/src/features/lobby/Lobby.js
@@ -11,38 +11,38 @@ export const Lobby = () => {
     null
   );
   console.log("Rendering Lobby", ringsDbInfo)
-  useEffect(() => {
-    const url = window.location.href;
-    if (url.includes("newroom")) {
-      if (url.includes("ringsdb") || url.includes("test")) {
-        var splitUrl = url.split( '/' );
-        const newroomIndex = splitUrl.findIndex((e) => e === "newroom")
-        const ringsDbDomain = splitUrl[newroomIndex + 1]
-        const newRingsDbInfo = [null, null, null, null];
-        var deckIndex = 0;
-        for (var i = newroomIndex + 2; i<splitUrl.length-1; i += 2 ) {
-          const ringsDbType = splitUrl[i];
-          const ringsDbId = splitUrl[i+1];
-          newRingsDbInfo[deckIndex] = {id: ringsDbId, type: ringsDbType, domain: ringsDbDomain};
-          deckIndex = deckIndex + 1;
-        }
-        setRingsDbInfo(newRingsDbInfo);
-      }
-      if (url.includes("replay")) {
-        var splitUrl = url.split( '/' );
-        const newroomIndex = splitUrl.findIndex((e) => e === "newroom")
-        setReplayId(splitUrl[newroomIndex + 2]);
-      }
-      setShowModal("createRoom");
-    }
-  }, []);
+  // useEffect(() => {
+  //   const url = window.location.href;
+  //   if (url.includes("newroom")) {
+  //     if (url.includes("ringsdb") || url.includes("test")) {
+  //       var splitUrl = url.split( '/' );
+  //       const newroomIndex = splitUrl.findIndex((e) => e === "newroom")
+  //       const ringsDbDomain = splitUrl[newroomIndex + 1]
+  //       const newRingsDbInfo = [null, null, null, null];
+  //       var deckIndex = 0;
+  //       for (var i = newroomIndex + 2; i<splitUrl.length-1; i += 2 ) {
+  //         const ringsDbType = splitUrl[i];
+  //         const ringsDbId = splitUrl[i+1];
+  //         newRingsDbInfo[deckIndex] = {id: ringsDbId, type: ringsDbType, domain: ringsDbDomain};
+  //         deckIndex = deckIndex + 1;
+  //       }
+  //       setRingsDbInfo(newRingsDbInfo);
+  //     }
+  //     if (url.includes("replay")) {
+  //       var splitUrl = url.split( '/' );
+  //       const newroomIndex = splitUrl.findIndex((e) => e === "newroom")
+  //       setReplayId(splitUrl[newroomIndex + 2]);
+  //     }
+  //     setShowModal("createRoom");
+  //   }
+  // }, []);
 
   const plugins = apiPlugins?.data?.data ? apiPlugins.data.data : null;
   console.log("pluginslist",plugins);
 
   return (
       <LobbyContainer>
-        <PluginsTable plugins={plugins} setSelectedPlugin={setSelectedPlugin}/>
+        <PluginsTable plugins={plugins}/>
       </LobbyContainer>
   );
 };
