311cee12b201781b44a214c8d75a675fa1f87824
seastan <chris.j.stanford@gmail.com>
2023-10-15 14:12:12 -0400

    minor bug fixes

--- frontend/src/features/engine/Browse.js
+++ frontend/src/features/engine/Browse.js
@@ -12,6 +12,7 @@ import { useGameDefinition } from "./hooks/useGameDefinition";
 import { useDoActionList } from "./hooks/useDoActionList";
 import { useLayout } from "./hooks/useLayout";
 import { convertToPercentage, getParentCardsInGroup } from "./functions/common";
+import { ca } from "date-fns/locale";
 
 const isNormalInteger = (val) => {
   var n = Math.floor(Number(val));
@@ -215,7 +216,7 @@ export const Browse = React.memo(({}) => {
               id="numFaceup"
               className="form-control w-full bg-gray-900 text-white border-0 h-full px-1 py-0"
               onChange={handleSelectClick}>
-              <option value="" disabled selected>{gameL10n("Turn faceup...")}</option>
+              <option value="" disabled selected>{gameL10n("Peek at...")}</option>
               <option value="None">{gameL10n("None")}</option>
               <option value="All">{gameL10n("All")}</option>
               <option value="5">{gameL10n("Top 5")}</option>
@@ -237,7 +238,7 @@ export const Browse = React.memo(({}) => {
         </div>
         {pairedFilterButtons.map((row, rowIndex) => {
           return(
-            <div className="h-1/5 w-full text-white text-center">
+            <div className="w-full text-white text-center" style={{height: `calc(100% / ${pairedFilterButtons.length+1})`}}>
               {row.map((item, itemIndex) => {
                 return(
                   <div className="h-full float-left w-1/2 p-0.5">
