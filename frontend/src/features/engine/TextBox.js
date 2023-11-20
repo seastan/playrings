8ef4f9ad8f76a95c009ca1f8291329a3e1f38d7c
seastan <chris.j.stanford@gmail.com>
2023-11-20 01:13:49 -0500

    added textbox

---
+++ frontend/src/features/engine/TextBox.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { convertToPercentage } from "./functions/common";
+import { useSelector } from "react-redux";
+
+export const TextBox = React.memo(({
+  textBoxLayoutInfo
+}) => {
+  const textBoxId = textBoxLayoutInfo?.textBoxId;
+  const textBox = useSelector(state => state?.gameUi?.game?.textBoxById?.[textBoxId]);
+  console.log("Rendering TextBox", textBoxLayoutInfo, textBox);
+  return (
+    <div 
+      className="absolute flex border border-gray-500 justify-center items-center text-gray-400 bg-gray-700" 
+      style={{
+        left: convertToPercentage(textBoxLayoutInfo.left), 
+        top: convertToPercentage(textBoxLayoutInfo.top), 
+        width: convertToPercentage(textBoxLayoutInfo.width), 
+        height: convertToPercentage(textBoxLayoutInfo.height),
+      }}>
+      {textBox?.content}
+    </div>
+  )
+})
