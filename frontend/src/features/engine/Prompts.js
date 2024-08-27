f11ec906512299a19aa94a2a7ffe6792f72f2bfa
seastan <chris.j.stanford@gmail.com>
2024-08-27 01:00:02 -0400

    add drag handle to prompt

--- frontend/src/features/engine/Prompts.js
+++ frontend/src/features/engine/Prompts.js
@@ -5,6 +5,8 @@ import { keysDiv } from "./functions/common";
 import { useDoActionList } from "./hooks/useDoActionList";
 import { useGameL10n } from "./hooks/useGameL10n";
 import Draggable from "react-draggable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGripLines } from "@fortawesome/free-solid-svg-icons";
 
 const promptStyle = {
   MozBoxShadow: '0 0 50px 20px black',
@@ -19,36 +21,41 @@ export const Prompts = React.memo(({
   const prompts = useSelector(state => state?.gameUi?.game?.playerData?.[playerN]?.prompts) || {};
   const sortedPromptIds = Object.keys(prompts).sort((a,b) => prompts[a].timestamp - prompts[b].timestamp);
   const doActionList = useDoActionList();
-  if (!prompts) return null;
+  if (Object.keys(prompts).length === 0) return null;
 
   return (
-    <Draggable>
-    <div className="absolute text-white" 
-      style={{
-        left: "2%", 
-        top: "2%", 
-        width: "19%",
-        zIndex: 3e7
-      }}>
-        {sortedPromptIds.map((promptKey, promptIndex) => {
-          return(
-            <div key={promptIndex} className="m-3 p-2 bg-gray-600-90 rounded" style={promptStyle}>
-              <div className="mb-2">{prompts[promptKey]["message"]}</div>
-                {prompts[promptKey]["options"] &&
-                  <div className="">
-                    {prompts[promptKey]["options"].map((option, index) => {
-                      return(
-                        <div key={index} className="m-1 p-1 rounded-lg bg-gray-800 hover:bg-red-800 cursor-default" onClick={() => doActionList(option.code)}>
-                          {promptIndex === 0 && <span className="pr-2">{keysDiv(option.hotkey, "hover:bg-gray-500")}</span>}<span>{gameL10n(option.label)}</span>
-                        </div>
-                      )
-                    })}
-                  </div>
-                }
-            </div>
-          )
-        })}
-  </div>
-  </Draggable>
+    <Draggable handle=".drag-handle">
+      <div className="absolute text-white" 
+        style={{
+          ...promptStyle,
+          left: "2%", 
+          top: "2%", 
+          width: "19%",
+          zIndex: 3e7
+        }}>
+          {/* Add a drag handle here */}
+          <div className="drag-handle p-1 cursor-move bg-gray-800 flex justify-center align-center">
+            <FontAwesomeIcon icon={faGripLines} />
+          </div>
+          {sortedPromptIds.map((promptKey, promptIndex) => {
+            return(
+              <div key={promptIndex} className="p-2 bg-gray-600-90" style={{borderBottom: "1px solid black"}}>
+                <div className="mb-2">{prompts[promptKey]["message"]}</div>
+                  {prompts[promptKey]["options"] &&
+                    <div className="">
+                      {prompts[promptKey]["options"].map((option, index) => {
+                        return(
+                          <div key={index} className="m-1 p-1 rounded-lg bg-gray-800 hover:bg-red-800 cursor-default" onClick={() => doActionList(option.code)}>
+                            {promptIndex === 0 && <span className="pr-2">{keysDiv(option.hotkey, "hover:bg-gray-500")}</span>}<span>{gameL10n(option.label)}</span>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  }
+              </div>
+            )
+          })}
+      </div>
+    </Draggable>
   )
 })
