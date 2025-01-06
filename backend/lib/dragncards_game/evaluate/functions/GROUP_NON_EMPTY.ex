d62d13a6208de410a8a150010a2329f259cbcbd7
seastan <chris.j.stanford@gmail.com>
2025-01-06 16:51:41 -0500

    added GROUP_EMPTY and GROUP_NOT_EMPTY

---
+++ backend/lib/dragncards_game/evaluate/functions/GROUP_NON_EMPTY.ex
@@ -0,0 +1,33 @@
+defmodule DragnCardsGame.Evaluate.Functions.GROUP_NOT_EMPTY do
+  alias DragnCardsGame.Evaluate
+  @moduledoc """
+  *Arguments*:
+  1. `groupId` (string)
+
+  *Returns*:
+  (boolean) True if the group is not empty, false otherwise.
+
+  """
+
+  @doc """
+  Executes the 'GROUP_NOT_EMPTY' operation with the given arguments.
+
+  ## Parameters
+
+    - `args`: The arguments required for the 'GROUP_NOT_EMPTY' operation.
+
+  ## Returns
+
+  The result of the 'GROUP_NOT_EMPTY' operation.
+  """
+  def execute(game, code, trace) do
+    group_id = Evaluate.evaluate(game, Enum.at(code, 1), trace)
+    cond do
+      # If group_id is not in game["groupById"], raise an error.
+      not Map.has_key?(game["groupById"], group_id) -> raise("GROUP_NOT_EMPTY: Group #{inspect(group_id)} does not exist.")
+      true -> Enum.count(game["groupById"][group_id]["stackIds"]) > 0
+    end
+  end
+
+
+end
